8f4605803c507aff352c934e19bcb7d000114cca
karbunkul <karbunkul@yourtask.ru>
2013-01-29 11:08:40 +0600

    template_preprocess_tgasu_extra_slider, tgasu-extra-slider template

--- tgasu-extra-slider.tpl.php
+++ tgasu-extra-slider.tpl.php
@@ -1,31 +1,18 @@
-
-
-<div id="main-banner" class="carousel slide">
-        <?php $banner_dir = 'http://localhost/devel/' . drupal_get_path('theme', 'tgasu') . '/banner'; ?>
-    <!-- Carousel items -->
-    <div class="carousel-inner">
-    	<div class="active item">
-    		<img alt="" src="<?php print $banner_dir; ?>/1.jpg">
-        <div class="carousel-caption">
-        	<h4>Первый слайд</h4>
-          	<p>Ну а тут можно выводить любой текст, в том числе и такие ссылки <a href="/node/1"><button class="btn btn-danger">подробнее</button></a></p></div>
-    		</div>
-    	<div class="item">
-    		<img alt="" src="<?php print $banner_dir; ?>/3.png">
-        <div class="carousel-caption">
-        	<h4>Второй слайд</h4>
-          	<p>Текст для второго слайда</p>
-        </div>
-    	</div>
-    	<div class="item">
-    		<img alt="" src="<?php print $banner_dir; ?>/2.jpg">
-        <div class="carousel-caption">
-        	<h4>Third Thumbnail label</h4>
-          	<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-        </div>
-    	</div>
-    </div>
-    <!-- Carousel nav -->
-    <a class="carousel-control left" href="#main-banner" data-slide="prev">&lsaquo;</a>
-    <a class="carousel-control right" href="#main-banner" data-slide="next">&rsaquo;</a>
-    </div>
+<div id="main-banner" class="carousel slide" style="max-width:940px;">
+  <!-- slider items -->
+  <div class="carousel-inner">
+	<?php foreach ($slides AS $key => $slide): ?>
+	  <?php dsm($slide); ?>
+		<div class="<?php $active == $key ? print "active " : print "" ?>item">
+		  <?php print $slide['content']; ?>
+			<div class="carousel-caption">
+			  <h4><?php print $slide['title']; ?></h4>
+			  <p><?php print $slide['description']; ?></p>
+			</div>
+		</div>
+	<?php endforeach; ?>
+  </div>
+  <!-- slider navigation -->
+  <a class="carousel-control left" href="#main-banner" data-slide="prev">&lsaquo;</a>
+  <a class="carousel-control right" href="#main-banner" data-slide="next">&rsaquo;</a>
+</div>
