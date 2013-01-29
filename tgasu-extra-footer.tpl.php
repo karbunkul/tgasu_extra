11aeba47097bae4294ff7e403522d19daa7940b0
karbunkul <karbunkul@yourtask.ru>
2013-01-29 17:17:01 +0600

    Add footer template and block

---
+++ tgasu-extra-footer.tpl.php
@@ -0,0 +1,22 @@
+<h1>FOOTER</h1>
+
+<div class="tabbable tabs-below">
+<div class="tab-content">
+<div id="tab1" class="tab-pane active">
+<div class="row-fluid">
+<div class="span3">1 колонка</div> 
+<div class="span3">2 колонка</div>
+<div class="span3">3 колонка</div>
+<div class="span3">4 колонка</div>
+</div>
+</div>
+<div id="tab2" class="tab-pane">
+<p>Здесь так же могут быть колонки</p>
+
+</div>
+<ul class="nav nav-tabs">
+<li class="active"><a data-toggle="tab" href="#tab1">Кафедры</a></li>
+<li><a data-toggle="tab" href="#tab2">Институты</a></li>
+</ul>
+</div>
+</div>
